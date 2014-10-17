de12df2971f372ba7793cb1965bf39b3d0815cb2
Code Division <codedivision@11th.ventures>
2014-10-17 15:40:11 +0800

    add email to teahers

--- db/migrate/20141017153211_add_email_to_teachers.rb
+++ db/migrate/20141017153211_add_email_to_teachers.rb
@@ -3,6 +3,9 @@ require_relative '../config'
 # this is where you should use an ActiveRecord migration to
 
 class AddEmailToTeachers < ActiveRecord::Migration
-  add_column :teachers, :email, :string
+
+  def change
+    add_column :teachers, :email, :string
+  end
 
 end
